f4e41e54f44de582e1b7f3fb10dd346ec6e07f72
Daniel Ferro <daniel.ferroc07@gmail.com>
2024-05-22 17:24:06 -0300

    modificação import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/common/constants/app_color.dart';
-import 'package:flutter_application_1/features/onboarding/onboardin_page.dart';
 import 'package:flutter_application_1/features/sign_up/sign_up_page.dart';
-import 'package:flutter_application_1/features/splash/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
